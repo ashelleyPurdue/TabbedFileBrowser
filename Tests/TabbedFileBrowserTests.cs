210b9b452f297053138580eb755588d0bb6704c3
Alex Shelley <ashelley@purdue.edu>
2018-11-04 14:43:42 -0500

    Wrote test StartsInWorkingDirectory

--- Tests/TabbedFileBrowserTests.cs
+++ Tests/TabbedFileBrowserTests.cs
@@ -12,6 +12,8 @@ namespace Tests
         const string TEMPLATE_PATH = "TestFilesTemplate";
         const string TEST_DIR_PATH = "TestFiles";
 
+        private ITabbedFileBrowserViewModel browser;
+
         [ClassInitialize]
         public static void ResetTestDir(TestContext context)
         {
@@ -27,11 +29,31 @@ namespace Tests
             Directory.SetCurrentDirectory(TEST_DIR_PATH);
         }
 
+        [TestInitialize]
+        public void MakeBrowser()
+        {
+            browser = new TabbedFileBrowserViewModel();
+
+            // TODO: Put this in a real control
+            // TODO: Put said control in a real window
+            // TODO: Close said window on test cleanup
+        }
+
         [TestMethod]
         public void TestFolderRestored()
         {
             string currentFolder = Directory.GetCurrentDirectory();
             Assert.AreEqual(TEST_DIR_PATH, Path.GetFileName(currentFolder));
+        }
+
+        [TestMethod]
+        public void StartsInWorkingDirectory()
+        {
+            string expected = Directory.GetCurrentDirectory();
+            expected = Path.GetFullPath(expected);
+
+            string actual = browser.CurrentTab.CurrentFolder;
+            Assert.AreEqual(expected, actual);
         }
     }
 }
