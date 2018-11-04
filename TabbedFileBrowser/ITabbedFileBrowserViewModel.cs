3cd8809608e105839f76469b811b59c7a503c8f2
Alex Shelley <ashelley@purdue.edu>
2018-11-04 11:11:05 -0500

    Created the interface for the view model.

---
+++ TabbedFileBrowser/ITabbedFileBrowserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TabbedFileBrowser
+{
+    public interface ITabbedFileBrowserViewModel
+    {
+        IReadOnlyList<ITabViewModel> Tabs { get; }
+        int SelectedTabIndex { get; set; }
+
+        void NewTab(string folderPath);
+        void CloseTab(int index);
+    }
+
+    public interface ITabViewModel
+    {
+        string CurrentFolder { get; }
+        string Title { get; }
+
+        IEnumerable<FileSystemInfo> VisibleFiles { get; }
+
+        void NavigateTo(string path);
+        void MoveUp();
+        void MoveForward();
+        void MoveBack();
+    }
+}
