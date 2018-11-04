f466ff67c77191e2c27608e18ca8fd39f88b8aa7
Alex Shelley <ashelley@purdue.edu>
2018-11-04 15:03:08 -0500

    Created stub view model for TabViewModel

---
+++ TabbedFileBrowser/TabViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabbedFileBrowser
+{
+    public class TabViewModel : ITabViewModel
+    {
+        public IEnumerable<FileSystemInfo> VisibleFiles => throw new NotImplementedException();
+
+        public string CurrentFolder { get; private set; }
+        public string Title         => throw new NotImplementedException();
+
+        public void MoveBack()      => throw new NotImplementedException();
+        public void MoveForward()   => throw new NotImplementedException();
+        public void MoveUp()        => throw new NotImplementedException();
+
+        public TabViewModel(string startFolder)
+        {
+            NavigateTo(startFolder);
+        }
+
+        public void NavigateTo(string path)
+        {
+            // TODO: Push this to a navigation stack of some sort
+            // instead of directly changing the folder
+            CurrentFolder = Path.GetFullPath(path);
+        }
+    }
+}
