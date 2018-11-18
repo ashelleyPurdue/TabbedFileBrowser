e02a65017bc579c5e4a1cc3b3818f4ec9778050b
Alex Shelley <ashelley@purdue.edu>
2018-11-17 20:43:46 -0500

    Changed TabTests to a partial class.  Moved all the close tab tests to a separate file.

---
+++ Tests/CloseTabTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabbedFileBrowser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestWindow;
+
+namespace Tests
+{
+    public partial class TabTests
+    {
+        private void AssertSelectedIndexChangeOnTabClose
+            (int numTabs, int startTab, int tabToClose, int expectedSelectedTab)
+        {
+            // Start by opening all the tabs
+            while (Browser.Tabs.Count < numTabs)
+                Browser.NewTab(".");
+
+            Browser.SelectedTabIndex = startTab;
+            Browser.CloseTab(tabToClose);
+
+            Assert.AreEqual(expectedSelectedTab, Browser.SelectedTabIndex);
+        }
+
+        [TestMethod]
+        public void DontCloseOnlyTab() => AssertSelectedIndexChangeOnTabClose
+        (
+            numTabs: 1,
+            startTab: 0,
+            tabToClose: 0,
+            expectedSelectedTab: 0
+        );
+
+        [TestMethod]
+        public void CloseTabAfterCurrent() => AssertSelectedIndexChangeOnTabClose
+        (
+            numTabs: 3,
+            startTab: 1,
+            tabToClose: 2,
+            expectedSelectedTab: 1
+        );
+
+        [TestMethod]
+        public void CloseTabBeforeCurrent() => AssertSelectedIndexChangeOnTabClose
+        (
+            numTabs: 3,
+            startTab: 1,
+            tabToClose: 0,
+            expectedSelectedTab: 0
+        );
+
+        [TestMethod]
+        public void CloseTabBeforeCurrentLast() => AssertSelectedIndexChangeOnTabClose
+        (
+            numTabs: 3,
+            startTab: 2,
+            tabToClose: 0,
+            expectedSelectedTab: 1
+        );
+
+        [TestMethod]
+        public void CloseCurrentTabFirst() => AssertSelectedIndexChangeOnTabClose
+        (
+            numTabs: 3,
+            startTab: 0,
+            tabToClose: 0,
+            expectedSelectedTab: 0
+        );
+
+        [TestMethod]
+        public void CloseCurrentTabMiddle() => AssertSelectedIndexChangeOnTabClose
+        (
+            numTabs: 3,
+            startTab: 1,
+            tabToClose: 1,
+            expectedSelectedTab: 1
+        );
+
+        [TestMethod]
+        public void CloseCurrentTabLast() => AssertSelectedIndexChangeOnTabClose
+        (
+            numTabs: 4,
+            startTab: 3,
+            tabToClose: 3,
+            expectedSelectedTab: 2
+        );
+    }
+}
