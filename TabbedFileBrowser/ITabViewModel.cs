1c73420f062d65c361f181fb581aa16d87f4a0f7
Alex Shelley <ashelley@purdue.edu>
2018-11-17 19:45:11 -0500

    Moved ITabViewModel to a separate file.

---
+++ TabbedFileBrowser/ITabViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabbedFileBrowser
+{
+    public interface ITabViewModel : INotifyPropertyChanged
+    {
+        string CurrentFolder { get; }
+        string Title { get; }
+
+        IEnumerable<FileSystemInfo> VisibleFiles { get; }
+
+        void NavigateTo(string path);
+        void MoveUp();
+        void MoveForward();
+        void MoveBack();
+        void Refresh();
+    }
+}
