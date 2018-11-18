17c47d9152bf52be905a826c31582020766c62bb
Alex Shelley <ashelley@purdue.edu>
2018-11-17 21:24:59 -0500

    Wrote tests FollowPathBackButtonAlwaysEnabled and MoveBackForwardAlwaysEnabled

--- Tests/FolderHistoryTests.cs
+++ Tests/FolderHistoryTests.cs
@@ -11,11 +11,62 @@ namespace Tests
 {
     public partial class TabTests
     {
+        private void AssertForwardBackButtons(bool back, bool forward)
+        {
+            Assert.AreEqual(back, Browser.CurrentTab.HasPrevFolder);
+            Assert.AreEqual(forward, Browser.CurrentTab.HasNextFolder);
+        }
+
         [TestMethod]
         public void NoForwardOrBackOnTabOpen()
+            => AssertForwardBackButtons(false, false);
+
+        [TestMethod]
+        public void FollowPathBackButtonAlwaysEnabled()
         {
-            Assert.IsFalse(Browser.CurrentTab.HasNextFolder);
-            Assert.IsFalse(Browser.CurrentTab.HasPrevFolder);
+            var tab = Browser.CurrentTab;
+
+            string[] folders = new[]
+            {
+                "folders",
+                "folders\\just_foo",
+                "folders\\just_bar"
+            };
+
+            // Browse through some folders
+            foreach (string f in folders)
+            {
+                tab.NavigateTo(f);
+                AssertForwardBackButtons(true, false);
+            }
+        }
+
+        [TestMethod]
+        public void MoveBackForwardAlwaysEnabled()
+        {
+            var tab = Browser.CurrentTab;
+
+            string[] folders = new[]
+            {
+                "folders",
+                "folders\\just_foo",
+                "folders\\just_bar"
+            };
+
+            // Browse through some folders
+            foreach (string f in folders)
+                tab.NavigateTo(f);
+
+            // Spam the back button
+            for (int i = folders.Length - 1; i >= 0; i--)
+            {
+                tab.MoveBack();
+
+                // The forward button should always be enabled.
+                // The back button should always be enabled, except for when
+                // we've reached the start of the history.
+                AssertForwardBackButtons(i != 0, true);
+            }
         }
     }
 }
