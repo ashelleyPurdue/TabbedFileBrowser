26fb5ac8a9afe7c095c6cfd2d0bfaf8c25bb86d8
Alex Shelley <ashelley@purdue.edu>
2018-11-22 18:35:55 -0500

    Added a description, company name, and version number to the assembly info.

--- TabbedFileBrowser/Properties/AssemblyInfo.cs
+++ TabbedFileBrowser/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TabbedFileBrowser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A multi-tabbed WPF control that lets you browse the file system.  Much like OpenFileDialogue, except it can be embedded in your form.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alex Shelley")]
 [assembly: AssemblyProduct("TabbedFileBrowser")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
