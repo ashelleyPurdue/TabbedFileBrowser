38f1f913f7a8b7148f5c943c440220bb3e1f6e7d
Alex Shelley <ashelley@purdue.edu>
2018-11-23 18:00:23 -0500

    wrote a test case.

--- Tests/FilterTests.cs
+++ Tests/FilterTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using TabbedFileBrowser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,14 +12,32 @@ namespace Tests
     [TestClass]
     public class FilterTests
     {
+        [TestClass]
+        public class StartsWithFilterTests : FilterTestsBase
+        {
+            protected override FilterCondition ParseFilterString(string filterString) =>
+                f => f.Name.StartsWith(filterString);
+
+            [TestMethod]
+            public void StartsWithFi() => AssertFiltered
+            (
+                folder: "fizz_buzz",
+                filter: "fi",
+                expectedFiles: new[] { "fizz.txt" }
+            );
+        }
     }
 
     public abstract class FilterTestsBase
     {
+        const string BASE_PATH = "../../TestFilesTemplate/folders";
+
         protected abstract FilterCondition ParseFilterString(string filterString);
 
         protected void AssertFiltered(string folder, string filter, params string[] expectedFiles)
         {
+            folder = Path.Combine(BASE_PATH, folder);
+
             ITabbedFileBrowserViewModel browser = new TabbedFileBrowserControl().ViewModel;
 
             browser.ParseFilterString = this.ParseFilterString;
