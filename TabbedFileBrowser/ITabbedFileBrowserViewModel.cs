0b6529f79783a95b6990d3a17d9d960cb7c42780
Alex Shelley <ashelley@purdue.edu>
2018-11-22 20:29:28 -0500

    The viewmodel now has a property for the selected index and selected file.  Bound them to the view.

--- TabbedFileBrowser/ITabbedFileBrowserViewModel.cs
+++ TabbedFileBrowser/ITabbedFileBrowserViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 
 namespace TabbedFileBrowser
 {
     public interface ITabbedFileBrowserViewModel : INotifyPropertyChanged
     {
         IReadOnlyList<ITabViewModel> Tabs { get; }
+
         int SelectedTabIndex { get; set; }
+
+        int SelectedFileIndex { get; set; }
+        FileSystemInfo SelectedFile { get; }
 
         ITabViewModel CurrentTab { get; }
 
