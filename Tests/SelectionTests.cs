9763384ba54f45b0b689eac2a4a8672d4ea27930
Alex Shelley <ashelley@purdue.edu>
2018-11-22 20:58:13 -0500

    Added unit test for SelectedIndexAndSelectedFileMatch

---
+++ Tests/SelectionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Linq;
+using System.IO;
+using TabbedFileBrowser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestWindow;
+
+namespace Tests
+{
+    public partial class TabTests
+    {
+        [TestMethod]
+        public void SelectedIndexAndSelectedFileMatch()
+        {
+            Browser.CurrentTab.NavigateTo("folders/fizz_buzz");
+            AssertVisibleFiles("buzz.txt", "fizz.txt");
+
+            Browser.SelectedFileIndex = 1;
+            Assert.AreEqual(Browser.SelectedFile.Name, "fizz.txt");
+        }
+    }
+}
