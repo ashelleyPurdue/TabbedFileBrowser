a97bab0f3cbc53dca876a4ddef94e7c2c4927906
Alex Shelley <ashelley@purdue.edu>
2018-11-23 18:05:35 -0500

    refactored it to remove the base class.  Wrote extra test cases for starting with bu and starting with empty string.

--- Tests/FilterTests.cs
+++ Tests/FilterTests.cs
@@ -12,35 +12,20 @@ namespace Tests
     [TestClass]
     public class FilterTests
     {
-        [TestClass]
-        public class StartsWithFilterTests : FilterTestsBase
-        {
-            protected override FilterCondition ParseFilterString(string filterString) =>
-                f => f.Name.StartsWith(filterString);
-
-            [TestMethod]
-            public void StartsWithFi() => AssertFiltered
-            (
-                folder: "fizz_buzz",
-                filter: "fi",
-                expectedFiles: new[] { "fizz.txt" }
-            );
-        }
-    }
-
-    public abstract class FilterTestsBase
-    {
         const string BASE_PATH = "../../TestFilesTemplate/folders";
 
-        protected abstract FilterCondition ParseFilterString(string filterString);
+        private FilterCondition StartsWith(string filterString)
+        {
+            return f => f.Name.StartsWith(filterString);
+        }
 
-        protected void AssertFiltered(string folder, string filter, params string[] expectedFiles)
+        private void AssertFiltered(string folder, string filter, params string[] expectedFiles)
         {
             folder = Path.Combine(BASE_PATH, folder);
 
             ITabbedFileBrowserViewModel browser = new TabbedFileBrowserControl().ViewModel;
 
-            browser.ParseFilterString = this.ParseFilterString;
+            browser.ParseFilterString = StartsWith;
             browser.CurrentTab.FilterString = filter;
             browser.CurrentTab.NavigateTo(folder);
 
@@ -48,10 +33,35 @@ namespace Tests
                                                 .Select(f => f.Name);
 
             // Sort them alphabetically before comparing them
-            var actualSorted   = actualFiles.OrderBy(f => f);
+            var actualSorted = actualFiles.OrderBy(f => f);
             var expectedSorted = expectedFiles.OrderBy(f => f);
 
             Assert.IsTrue(actualSorted.SequenceEqual(expectedSorted));
         }
+
+
+        [TestMethod]
+        public void StartsWithFi() => AssertFiltered
+        (
+            folder: "fizz_buzz",
+            filter: "fi",
+            expectedFiles: new[] { "fizz.txt" }
+        );
+
+        [TestMethod]
+        public void StartsWithBu() => AssertFiltered
+        (
+            folder: "fizz_buzz",
+            filter: "bu",
+            expectedFiles: new[] { "buzz.txt" }
+        );
+
+        [TestMethod]
+        public void StartsWithEmptyStr() => AssertFiltered
+        (
+            folder: "fizz_buzz",
+            filter: "",
+            expectedFiles: new[] { "buzz.txt", "fizz.txt" }
+        );
     }
 }
